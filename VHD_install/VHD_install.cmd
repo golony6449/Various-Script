f710c38694a0165494c4f808f8fa107c3bf800ae
golony6449 <bb0162@live.co.kr>
2018-07-05 16:31:01 +0900

    기존 이미지 사용 여부 분기점 추가

--- VHD_install/VHD_install.cmd
+++ VHD_install/VHD_install.cmd
@@ -3,13 +3,14 @@
 echo.
 echo.
 echo.
-echo		VHD 설정 스크립트 (Part 1)
+echo		VHD 설정 스크립트
 echo.
 echo.
 echo.
+
+:: SSD, HDD 여부 설정
 :restart
 set /p isSSD=SSD PC 입니까? (y/n)
-
 
 if "%isSSD%" == "y" (
 echo	SSD 환경에서의 설정을 진행합니다.
@@ -18,9 +19,32 @@ if "%isSSD%" == "n" (
 echo	HDD 환경에서의 설정을 진행합니다.
 ) else (
 echo	올바르지 않은 입력입니다.
-echo.
 goto restart
 )
+)
+
+:: 기존 이미지 사용 여부 확인
+:imgcheck
+if exist C:\vhd\windows.vhd (
+echo	이미지가 존재합니다.
+echo.
+
+set /p oldimg="기존 이미지를 사용하겠습니까? (y/n)"
+
+if "%oldimg%" == "y" (
+    echo    기존 이미지를 사용합니다.
+    goto vhd
+) else (
+    if "%oldimg%" == "n" (
+        echo    스토리지를 초기화 하고 새 이미지를 복사합니다.
+        goto start
+    ) 
+)
+) else (
+echo	이미지가 존재하지 않습니다. 계속 진행합니다.
+set oldimg=n
+)
+
 
 :start
 echo.
@@ -43,21 +67,22 @@ echo	부트로더 설치 완료
 echo.
 echo.
 echo.
-
-if exist C:\vhd\windows.vhd (
-echo	이미지가 존재합니다.
-) else (
+if "%oldimg%" == "n" (
 echo	이미지를 복사합니다.
 robocopy .\vhd c:\vhd /ETA
 )
 
+:: VHD 작업 파트
+:vhd
 echo.
 echo.
 echo.
 echo.
 echo	VHD 작업 시작
 echo.
-if %isSSD% == "y" (
+echo    VHD 용량을 확장합니다.
+echo.
+if "%isSSD%" == "y" (
 
 echo	부모를 Attach한 뒤, V 문자를 할당합니다.
 echo.
